ca38b13374953814783fde57e0c17b31d9ac6e38
Parth Dalwadi <dalwadip77@gmail.com>
2023-04-15 18:44:09 -0400

    Added basic icon to game window instead of using the default icon.

--- src/main/App.java
+++ src/main/App.java
@@ -1,12 +1,16 @@
 package main;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class App{
+	public static JFrame window;
+	
 	public static void main(String[] args){
-		JFrame window = new JFrame();
+		window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setTitle("Snake Game!");
+		new App().setIcon();
 		window.setResizable(false);
 		GamePanel gamePanel = new GamePanel();
 		window.add(gamePanel);
@@ -14,5 +18,10 @@ public class App{
 		window.setLocationRelativeTo(null);
 		window.setVisible(true);
 		gamePanel.startGameThread();
+	}
+	
+	public void setIcon() {
+		ImageIcon icon = new ImageIcon(getClass().getClassLoader().getResource("icon/icon.png"));
+		window.setIconImage(icon.getImage());
 	}
 }
